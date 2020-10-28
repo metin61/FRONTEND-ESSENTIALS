6bfaf87314ddf2d050929f54a669fe207a0e0127
metin celik <metin.celik@student.rocva.nl>
2020-10-28 12:18:08 +0100

    meerdere extensies

--- 01 - Nog meer tags/taak07 - Meerdere extensies/script.js
+++ 01 - Nog meer tags/taak07 - Meerdere extensies/script.js
@@ -1,10 +1,1 @@
-<!doctype html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <title>Meedere bestanden en extenties</title>
-    </head>
-    <body>
-        <h1>Hello world!</h1>
-        <script src="script.js"></script> <!-- het bestand is een extern JS bestand -->
-    </body>
+alert('de links werkt!')
---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,18 @@
+console.log (mijnVoornaam = "Metin")
+console.log (mijnAchternaam = "Celik")
+console.log (mijnLeeftijd = "22")
+console.log (ikBenStemGerechtig);
+console.log (ikMagLegaalAlcoholKopen);
+console.log (ikMagBrommerRijden);
+console.log (SoftwareOntwikkeling);
+
+
+
+
+
+
+
+
+
+
+
