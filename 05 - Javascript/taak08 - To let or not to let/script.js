87fa84ba9596da65f69d5926852ddda937936fd1
metin celik <metin.celik@student.rocva.nl>
2020-11-03 14:17:40 +0100

    rekenen

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,9 @@
+let huidigJaartal = 2020
+let geboorteJaar = 1998
+let mijnLeeftijd = 22
+leeftijd = (huidigJaartal) - (geboorteJaar);
+console.log(huidigJaartal);
+console.log(geboorteJaar);
+console.log(mijnLeeftijd);
+console.log(leeftijd);
+
