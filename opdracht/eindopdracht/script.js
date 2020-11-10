87286023d248c96cc1f92b31f02fa7418c8306f1
metin celik <metin.celik@student.rocva.nl>
2020-11-10 12:20:51 +0100

    eindopdracht

--- opdracht/eindopdracht/script.js
+++ opdracht/eindopdracht/script.js
@@ -1,17 +1,19 @@
-let voornaam = prompt("Wat is je voornaam?");
-let achternaam = prompt("Wat is je achternaam?");
-let  email = prompt("Wat is je email?");
-let  leeftijd = prompt("Wat is je leeftijd?");
-let  woonplaats = prompt("Wat is je woonplaats?");
+var bezoeker = {
+ voornaam : prompt("Wat is je voornaam?"),
+ achternaam : prompt("Wat is je achternaam?"),
+ email : prompt("Wat is je email?"),
+ leeftijd : prompt("Wat is je leeftijd?"),
+ woonplaats : prompt("Wat is je woonplaats?"),
+};
 
-document.querySelector("#voornaam").innerHTML = voornaam;
-document.querySelector("#achternaam").innerHTML = achternaam;
-document.querySelector("#email").innerHTML = email;
-document.querySelector("#leeftijd").innerHTML = leeftijd;
-document.querySelector("#woonplaats").innerHTML = woonplaats;
+document.querySelector("#voornaam").innerHTML = bezoeker.voornaam;
+document.querySelector("#achternaam").innerHTML = bezoeker.achternaam;
+document.querySelector("#email").innerHTML = bezoeker.email;
+document.querySelector("#leeftijd").innerHTML = bezoeker.leeftijd;
+document.querySelector("#woonplaats").innerHTML = bezoeker.woonplaats;
 
     
-document.querySelector("#kopjenaam").innerHTML = "Welkom" + " " + voornaam + " " + achternaam;
+document.querySelector("#kopjenaam").innerHTML = "Welkom" + " " + bezoeker.voornaam + " " + bezoeker.achternaam;
 
 if(leeftijd == "16") {
     document.querySelector(".midden").style.backgroundColor = 'orange';
